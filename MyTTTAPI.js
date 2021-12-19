b75c0ca39fd0fd1d51d85194aa2e04ee41c18777
CeniusM <cenius.meier@gmail.com>
2021-12-19 13:15:53 +0100

    changes

---
+++ MyTTTAPI.js
@@ -0,0 +1,17 @@
+// All this is for is to get an inpput from the html, run through js and output it through html or css, idk
+
+let Click_Amount = 0;
+function Click() {
+  //   alert("Hallo World");
+  Click_Amount++;
+  Color();
+}
+
+function Color() {
+  if (Click_Amount % 2 === 0) {
+    console.log("Nice by 2"); // Color blue with css
+    document.querySelector("title 2 click");
+  } else {
+    document.querySelector("one click my friend :D");
+  } // red
+}
